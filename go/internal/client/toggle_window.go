d8673caabea41f691ac950e3c95c1539fb8f5f6f
VinewZ <vinewalves@hotmail.com>
2025-06-23 19:20:40 -0300

    update toggle window request

--- go/internal/client/toggle_window.go
+++ go/internal/client/toggle_window.go
@@ -14,11 +14,13 @@ type ToggleWindowClient struct {
 	TimeoutMS int
 }
 
-func (c *ToggleWindowClient) ToggleWindow(ctx context.Context) error {
+func (c *ToggleWindowClient) ToggleWindow(ctx context.Context, isVisible bool) error {
 	ctx, cancel := context.WithTimeout(ctx, time.Duration(c.TimeoutMS)*time.Millisecond)
 	defer cancel()
 
-	req := connect.NewRequest(&pb.ToggleWindowRequest{})
+	req := connect.NewRequest(&pb.ToggleWindowRequest{
+		IsVisible: isVisible,
+	})
 	_, err := c.Stub.ToggleWindow(ctx, req)
 	return err
 }
